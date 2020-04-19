fd5a2bc4c6ce7bc709785ce4e05da439f3f678fc
Tommy Han <tommy@Tommys-Air.home>
2020-04-19 17:24:56 -0400

    added new files and setup eslint and package.json

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
+import Calendar from "./js/Calendar.js";
 
-const Pet = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese")
-  ]);
-};
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      username: "Hello Tommy",
+    };
+  }
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
-  ]);
-};
+  render() {
+    return (
+      <div className="button__container">
+        <button className="button" onClick={this.handleClick}>
+          Click Me
+        </button>
+        <p>{this.state.username}</p>
+      </div>
+    );
+  }
 
-render(React.createElement(App), document.getElementById("root"));
+  componentDidMount() {
+    console.log("componentDidMount");
+    fetch("https://curriculum-api.codesmith.io/messages")
+      .then((data) => data.json())
+      .then((messages) => {
+        console.log(messages);
+        this.appendMessages(messages);
+      });
+  }
+
+  // append data
+  appendMessages(messages) {
+    console.log(`appendMessages`);
+    const messagesArray = [];
+    for (let i = 0; i < messages.length; i++) {
+      const message =
+        messages[i].created_at.slice(0, -8).replace("T", " ") +
+        " : [" +
+        messages[i].created_by +
+        "] - " +
+        messages[i].message;
+      messagesArray.push(message);
+    }
+    return messagesArray;
+  }
+}
+
+render(<App />, document.querySelector("#root"));
---
+++ src/js/Calendar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Calendar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Calendar</h1>
+      </div>
+    );
+  }
+}
